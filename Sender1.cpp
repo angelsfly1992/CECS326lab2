fdd1f8971f45f7b4c1ae35802cd96c6c5a8d3de0
Bunly Buth <angelsfly1992@yahoo.com>
2018-02-27 19:53:15 -0800

    Create Sender1.cpp

---
+++ Sender1.cpp
@@ -0,0 +1,19 @@
+#define MSG_TYPE_ACK 1
+#define MSG_TYPE_DATA 2
+#define MSG_TYPE_TERM 1
+
+using namespace std
+
+
+struct buf {
+	int mtype;
+	int data;
+);
+
+//current sender type
+int sender_type=0;
+
+
+//random 100 numbers value
+int r = (rand()% 100+ 1;
+cout << Random Value: " <<r<<endl;
