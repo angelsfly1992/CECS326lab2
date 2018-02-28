0b675a354ffdb717e01eed21df278b8317754f06
Bunly Buth <angelsfly1992@yahoo.com>
2018-02-27 20:37:52 -0800

    Update Sender1.cpp

--- Sender1.cpp
+++ Sender1.cpp
@@ -20,3 +20,13 @@ void sig_handler(int);
 //random 100 numbers value
 int r = (rand()% 100+ 1;
 cout << Random Value: " <<r<<endl;
+message_buff sbuf;
+sbuf.data = rand
+
+//Check if the sender type is 997 if so terminal if number is less than 100
+if(sender_type == 997 && r<100) {
+	sbuf.mtype = MSG_TYPE_TERM;
+}
+else {
+	sbuf.mtype = MSG_TYPE_DATA;	
+}
